2d8a685d01016251e8f3ae1ff9447135c2a924f7
oouxx <wangxinxing961129@gmail.com>
2026-07-06 09:49:18 +0800

    Add openatrading cask v0.12.7

---
+++ Casks/openatrading.rb
@@ -0,0 +1,31 @@
+cask "openatrading" do
+  version "0.12.7"
+  sha256 :no_check
+
+  if Hardware::CPU.intel?
+    url "https://github.com/oouxx/openatrading/releases/download/v#{version}/openatrading_#{version}_x64.dmg"
+  else
+    url "https://github.com/oouxx/openatrading/releases/download/v#{version}/openatrading_#{version}_aarch64.dmg"
+  end
+
+  name "OpenATrading"
+  desc "AI-native A股短线交易决策工作台"
+  homepage "https://github.com/oouxx/openatrading"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+
+  app "OpenATrading.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.openatrading.app",
+    "~/Library/Caches/com.openatrading.app",
+    "~/Library/Preferences/com.openatrading.app.plist",
+    "~/Library/Saved Application State/com.openatrading.app.savedState",
+    "~/Library/WebKit/com.openatrading.app",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,28 @@
+# Homebrew OpenATrading Tap
+
+通过 Homebrew Cask 安装 OpenATrading。
+
+## 安装
+
+```bash
+brew tap oouxx/openatrading
+brew install --cask openatrading
+```
+
+## 升级
+
+```bash
+brew update
+brew upgrade --cask openatrading
+```
+
+## 卸载
+
+```bash
+brew uninstall --cask openatrading
+brew untap oouxx/openatrading
+```
+
+## 链接
+
+- [OpenATrading](https://github.com/oouxx/openatrading)
